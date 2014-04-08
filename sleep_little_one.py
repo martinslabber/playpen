907223034034a2ba79c52f31add9e61ede294240
Martin Slabber <martin.slabber@gmail.com>
2014-04-08 15:51:31 +0200

    Update sleep little one

--- sleep_little_one.py
+++ sleep_little_one.py
@@ -4,7 +4,7 @@ import random
 import time
 
 while True:
-    space = random.randint(2,10)
-    print "\033[9%dm" % (space), " " * space , "la la"
+    space = random.randint(2, 10)
+    print "\033[9%dm" % (space), " " * space, "la la little kittie"
     time.sleep(0.2)
 
